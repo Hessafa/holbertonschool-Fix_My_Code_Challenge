17513b87f48f3f3afe98219407052c578a262210
Hussah ✨ <10685@holbertonstudents.com>
2025-09-28 18:11:58 +0300

    Create 0-fizzbuzz.py

---
+++ challenge/0-fizzbuzz.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import sys
+
+def fizzbuzz(n):
+    out = []
+    for i in range(1, n+1):
+        if i % 15 == 0:
+            out.append("FizzBuzz")
+        elif i % 3 == 0:
+            out.append("Fizz")
+        elif i % 5 == 0:
+            out.append("Buzz")
+        else:
+            out.append(str(i))
+    print(" ".join(out))
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2 or not sys.argv[1].isdigit():
+        sys.exit(1)
+    n = int(sys.argv[1])
+    fizzbuzz(n)
